e657d0425e6a685c9158af21a37d1330da988b80
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-07-30 21:43:39 +0900

    memoの基本を学習。

--- src/components/ChildArea.js
+++ src/components/ChildArea.js
@@ -1,15 +1,27 @@
-const ChildArea = (props) => {
+import { memo } from 'react';
+//propsが変更されない限り再レンダリングしないようにするのがmemo
+const ChildArea = memo((props) => {
   const { open, text } = props;
+  const data = [...Array(2000).keys()];
+  console.log('childArea is rendered!');
+
+  //再レンダリングされるタイミング
+  // stateが更新された時
+  // 親から渡されるpropsが変更された時
+  // 親コンポーネントが再レンダリングされた時
   return (
     <>
       {open && (
         <div>
           <h1>child component</h1>
           <h3>{text}</h3>
+          {data.map((aData) => (
+            <p key={aData}>{aData}</p>
+          ))}
         </div>
       )}
     </>
   );
-};
+});
 
 export default ChildArea;
