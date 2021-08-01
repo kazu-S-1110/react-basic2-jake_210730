1b24920a9e883d9cd6d5d3f7ad9b45b8a8415ab4
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-01 13:54:33 +0900

    react-router-domの基本を理解。

--- src/App.js
+++ src/App.js
@@ -7,6 +7,10 @@ import CssModules from './components/CssModules';
 import StyledJsx from './components/StyledJsx';
 import StyledComponents from './components/StyledComponents';
 import Emotion from './components/Emotion';
+import Home from './Home';
+import Page1 from './Page1';
+import Page2 from './Page2';
+import { BrowserRouter, Link, Switch, Route } from 'react-router-dom';
 
 function App() {
   const [count, setCount] = useState(0);
@@ -19,22 +23,44 @@ function App() {
   const handleClose = useCallback(() => setOpen(false), []);
   //関数をメモ化するのがusecallback
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>{count}</p>
-        <button onClick={countUp}>Count UP</button>
-        <br />
-        <input type="text" onChange={(e) => setText(e.target.value)} />
-        <button onClick={() => setOpen(!open)}>display</button>
-        <ChildArea open={open} handleClose={handleClose} />
-        <InlineStyle />
-        <CssModules />
-        <StyledJsx />
-        <StyledComponents />
-        <Emotion />
-      </header>
-    </div>
+    <BrowserRouter>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{count}</p>
+          <button onClick={countUp}>Count UP</button>
+          <br />
+          <input type="text" onChange={(e) => setText(e.target.value)} />
+          <button onClick={() => setOpen(!open)}>display</button>
+          <ChildArea open={open} handleClose={handleClose} />
+          {open && (
+            <>
+              <InlineStyle />
+              <CssModules />
+              <StyledJsx />
+              <StyledComponents />
+              <Emotion />
+            </>
+          )}
+          {/* urlを変えてくれる */}
+          <Link to="/">Home</Link>
+          <Link to="/page1">Page1</Link>
+          <Link to="/page2">Page2</Link>
+          {/* urlに応じてコンポーネントの表示を切り替えるのは以下のコード、exactを指定しないと他のものとマッチしないので注意。 */}
+          <Switch>
+            <Route exact path="/">
+              <Home />
+            </Route>
+            <Route path="/page1">
+              <Page1 />
+            </Route>
+            <Route path="/page2">
+              <Page2 />
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </BrowserRouter>
   );
 }
 
---
+++ src/Page1.jsx
@@ -0,0 +1,9 @@
+const Page1 = () => {
+  return (
+    <div>
+      <h1>This is Page1 page!</h1>
+    </div>
+  );
+};
+
+export default Page1;
