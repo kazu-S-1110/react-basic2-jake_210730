97ccac02f25b1b28120323f6c8cb9e0c03d54184
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-02 12:57:17 +0900

    クエリパラメータの取得方法を学習。

--- src/Page2.jsx
+++ src/Page2.jsx
@@ -1,10 +1,14 @@
 import { Link } from 'react-router-dom';
 const Page2 = () => {
   const urlTest = '3gaga44';
+  const queryParams = 'hogehoge';
+
   return (
     <div>
       <h1>This is Page2 page!</h1>
       <Link to={`/page2/${urlTest}`}>URL Parameter</Link>
+      <br />
+      <Link to={`/page2/${urlTest}?name=${queryParams}`}>Query Parameter</Link>
     </div>
   );
 };
--- src/UrlParameter.jsx
+++ src/UrlParameter.jsx
@@ -1,7 +1,13 @@
-import { useParams } from 'react-router-dom';
+import { useParams, useLocation } from 'react-router-dom';
 
 const UrlParameter = () => {
   const { id } = useParams(); //useParamsを使うことでURLパラメータを取得することが可能
+  // const location = useLocation(); //useLocationを使うことでクエリパラメータを取得することが可能
+  // console.log(location);
+  const { search } = useLocation(); //この状態では?name=hogehogeとなるのでURLsearchParamsを使用する
+  const query = new URLSearchParams(search);
+  console.log(query.get('name')); //これでhogehogeが取得できる
+
   const container = {
     border: 'solid 1px red',
   };
@@ -9,7 +15,12 @@ const UrlParameter = () => {
   return (
     <div style={container}>
       <h3>This is UrlParameter</h3>
-      <p>{id}</p>
+      <p>
+        this parameter is <strong>{id}</strong>
+      </p>
+      <p>
+        query parameter is <strong>{query.get('name')}</strong>
+      </p>
     </div>
   );
 };
