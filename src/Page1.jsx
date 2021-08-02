ac5093b30190b5ffbb369d790053f8e7ab0ae587
kazu-S-1110 <67362767+kazu-S-1110@users.noreply.github.com>
2021-08-02 18:43:46 +0900

    useHistoryの使い方を学習。これはもっと早く知りたかった！！

--- src/Page1.jsx
+++ src/Page1.jsx
@@ -1,8 +1,9 @@
-import { Link } from 'react-router-dom';
+import { Link, useHistory } from 'react-router-dom';
 const Page1 = () => {
   const arr = [...Array(100).keys()];
   console.log(arr);
-
+  // Linkを使わずにページ遷移をする方法、結構使う場面ありそう。
+  const history = useHistory();
   return (
     <div>
       <h1>This is Page1!</h1>
@@ -10,6 +11,8 @@ const Page1 = () => {
       <Link to={{ pathname: '/page1/detailA', state: arr }}>DetailA</Link>
       <br />
       <Link to="/page1/detailB">DetailB</Link>
+      <br />
+      <button onClick={() => history.push('/page1/detailA')}>DetailA</button>
     </div>
   );
 };
--- src/Page1DetailA.jsx
+++ src/Page1DetailA.jsx
@@ -1,13 +1,16 @@
-import { useLocation } from 'react-router-dom';
+import { useHistory, useLocation } from 'react-router-dom';
 
 const Page1DetailA = () => {
   // const location = useLocation();
   // console.log(location); //ページ遷移で渡されたものはこれで受け取る
   const { state } = useLocation();
+  const history = useHistory();
   console.log(state);
   return (
     <div>
       <h1>This is Page1 Detail A</h1>
+      {/* historyのgoBack関数、めっちゃ使えそう！ */}
+      <button onClick={() => history.goBack()}>GoBack!</button>
     </div>
   );
 };
